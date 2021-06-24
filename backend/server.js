d8340789c52f05178e989e43152aefcb72c8321b
Adeel Ahsan <adeelahsan5640@gmail.com>
2021-06-24 11:39:20 -0700

    update

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,12 @@
-const express = require('express')
-const dotenv = require('dotenv')
-const app = express()
-const products = require('./data/products')
+import express from 'express'
+import dotenv from 'dotenv'
+import colors from 'colors'
+import connectDB from './config/db.js'
+import products from './data/products.js'
 
 dotenv.config()
+connectDB()
+const app = express()
 
 app.get('/', (req, res) => {
   res.send('API is running...')
@@ -18,5 +21,7 @@ app.get('/api/products/:id', (req, res) => {
 const PORT = process.env.PORT || 5000
 app.listen(
   PORT,
-  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
+  console.log(
+    `Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold
+  )
 )
