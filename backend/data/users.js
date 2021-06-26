cfaef328b2e1649f483a2aea690efb5c0906fe27
Adeel Ahsan <adeelahsan5640@gmail.com>
2021-06-26 08:15:34 -0700

    update

---
+++ backend/data/users.js
@@ -0,0 +1,20 @@
+import bcrypt from 'bcryptjs'
+const users = [
+  {
+    name: 'Admin User',
+    email: 'admin@example.com',
+    password: bcrypt.hashSync('123456', 10),
+    isAdmin: true,
+  },
+  {
+    name: 'Adeel Ahsan',
+    email: 'adeel@gmail.com',
+    password: bcrypt.hashSync('123456', 10),
+  },
+  {
+    name: 'Talha Hussain',
+    email: 'thb@gmail.com',
+    password: bcrypt.hashSync('123456', 10),
+  },
+]
+export default users
